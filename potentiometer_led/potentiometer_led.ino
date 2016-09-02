0304a1969638643f74ea0c3de927468eed5351fc
Denis Nuțiu <nutiudenis@hotmail.com>
2016-09-02 20:58:17 +0300

    Added potentiometer led

---
+++ potentiometer_led/potentiometer_led.ino
@@ -0,0 +1,23 @@
+
+/*
+ * Date:   02.09.2016
+ * Author: Denis Nutiu
+ */
+#define POTENTIOMETER_PIN 5
+#define LED_PIN 3
+
+int potentiometer_value;
+
+void setup() {
+    Serial.begin(9600);
+    Serial.println("Initialized");
+    pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  potentiometer_value = analogRead(POTENTIOMETER_PIN) / 4;
+  potentiometer_value = constrain(potentiometer_value, 0, 255);
+  Serial.println(potentiometer_value);
+  analogWrite(LED_PIN, potentiometer_value);
+  delay(10);
+}
